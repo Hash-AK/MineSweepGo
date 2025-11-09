13d30892949e6e1fcd788f0492115c598f60a56c
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-11-09 15:43:10 -0500

    added mines counter

--- main.go
+++ main.go
@@ -62,6 +62,8 @@ func printString(screen tcell.Screen, x, y int, style tcell.Style, str string) {
 func main() {
 	selectedX := 0
 	selectedY := 0
+	totalMines := 0
+	flagNumbers := 0
 	screen, err := tcell.NewScreen()
 	if err != nil {
 		fmt.Printf("Error initializing screen : %s\n", err)
@@ -104,6 +106,7 @@ func main() {
 			randSeed := rand.IntN(5)
 			if randSeed == 0 {
 				game.grid[r][c].isMine = true
+				totalMines = totalMines + 1
 
 			}
 		}
@@ -188,6 +191,7 @@ func main() {
 		termWidth, termHeight := screen.Size()
 		startX := termWidth/2 - widthInt
 		startY := termHeight/2 - heightInt/2
+
 		logoX := termWidth/2 - len(" |_|  |_|_|_||_\\___|___/\\_/\\_/\\___\\___| .__/\\___\\___/")/2
 		printString(screen, logoX, startY-6, logoStyle, "  __  __ _          ___                      ___     ")
 		printString(screen, logoX, startY-5, logoStyle, " |  \\/  (_)_ _  ___/ __|_ __ _____ ___ _ __ / __|___ ")
@@ -197,10 +201,14 @@ func main() {
 		instructions := "Arrow key to move selection | 'F' to flag | Enter to reveal"
 		instructionsX := termWidth/2 - len(instructions)/2
 		printString(screen, instructionsX, startY+heightInt+2, logoStyle, instructions)
-		credits := "By github.com/Hash-AK"
+		credits := "By: github.com/Hash-AK"
 		creditsX := termWidth/2 - len(credits)/2
 		printString(screen, creditsX, startY+heightInt+4, linkStyle, credits)
 		game.grid[selectedY][selectedX].isSelected = true
+		status := fmt.Sprintf("Flags Place/Total Mines : %d, %d", flagNumbers, totalMines)
+		statusX := startX + widthInt*2 + 5
+		statusY := termHeight / 2
+		printString(screen, statusX, statusY, logoStyle, status)
 		screen.SetContent(startX-2, startY-1, '╭', nil, defstyle)
 		screen.SetContent(startX+widthInt*2, startY-1, '╮', nil, defstyle)
 
@@ -296,8 +304,10 @@ func main() {
 				if ev.Rune() == 'f' {
 					if game.grid[selectedY][selectedX].isFlagged == false {
 						game.grid[selectedY][selectedX].isFlagged = true
+						flagNumbers = flagNumbers + 1
 					} else {
 						game.grid[selectedY][selectedX].isFlagged = false
+						flagNumbers = flagNumbers - 1
 					}
 
 				}
